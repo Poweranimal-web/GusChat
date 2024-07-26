81767e301a309adc18f7eb33f68b4cada464158e
Poweranimal-web <nikita.guseff@gmail.com>
2024-07-26 11:45:20 +0300

    first commit for VOIP feature which is written by C++

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <portaudio.h>
+#include <boost-1_85/boost/asio.hpp>
+using namespace std;
+using namespace boost::asio;
+using ip::tcp;
+
+int main() {
+    
+
+    return 0;
+}
